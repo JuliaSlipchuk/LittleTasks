d50fa8ffaa3d83f55852a492aa3d15a690fe1694
JuliaSlipchuk <julia2015olex@gmail.com>
2020-05-01 09:35:58 +0300

    game with switches

---
+++ game with switches/game with switches/InputData.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game_with_switches
+{
+    class InputData
+    {
+        public int TestsCount { get; set; }
+        public List<int> Tests { get; set; }
+
+        public void GetInputData()
+        {
+            int count;
+            Console.Write("Enter tests count: ");
+            if (Int32.TryParse(Console.ReadLine(), out count) && count > 0)
+            {
+                this.TestsCount = count;
+            }
+            else
+            {
+                this.TestsCount = 1;
+            }
+            this.Tests = new List<int>();
+            int test;
+            for (int i = 1; i <= count; i++)
+            {
+                Console.Write($"Enter {i} test: ");
+                if (Int32.TryParse(Console.ReadLine(), out test) && test > 0)
+                {
+                    this.Tests.Add(test);
+                }
+                else
+                {
+                    i--;
+                    continue;
+                }
+            }
+        }
+        public void GetSwitchState()
+        {
+            bool state;
+            string result;
+            for (int i = 0; i < this.TestsCount; i++)
+            {
+                state = false;
+                for (int j = 1; j <= this.Tests[i]; j++)
+                {
+                    if (this.Tests[i] % j == 0)
+                    {
+                        state = !state;
+                    }
+                    Console.WriteLine($"{j} - {state}");
+                }
+                result = state ? string.Format("Switch {0} is ON", this.Tests[i]) :  string.Format("Switch {0} is OFF", this.Tests[i]);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ game with switches/game with switches/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game_with_switches
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InputData data = new InputData();
+            data.GetInputData();
+
+            data.GetSwitchState();
+            Console.ReadKey();
+        }
+    }
+}
