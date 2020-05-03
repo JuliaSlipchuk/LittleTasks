d3e2827f5edbc4ad73e7dc883d72865581a28a49
JuliaSlipchuk <julia2015olex@gmail.com>
2020-05-03 12:44:13 +0300

    my favorite and irresistible

---
+++ my favorite and irresistible/My favorite and irresistible/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_favorite_and_irresistible
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int denominator;
+            do
+            {
+                Console.Write("Enter denominator: ");
+            }
+            while (!Int32.TryParse(Console.ReadLine(), out denominator) || denominator < 1);
+            int countOfFractions = GetCountOfFractions(denominator);
+            Console.WriteLine($"Count of fractions: {countOfFractions}");
+
+            Console.ReadKey();
+        }
+        private static int GetCountOfFractions(int denominator)
+        {
+            int count = 0;
+            if (denominator == 1)
+            {
+                return 0;
+            }
+            else
+            {
+                for (int i = denominator; i > 1; i--)
+                {
+                    for (int j = i - 1; j > 0; j--)
+                    {
+                        if (j != 1 && i % j == 0)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
